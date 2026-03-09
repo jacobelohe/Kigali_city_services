ad0790e8e33309538b736389f910d225f79fd057
elohejacs <elohe.jacs@gmail.com>
2026-03-09 19:42:45 +0200

    Add inline documentation to auth and listing providers

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,3 +1,4 @@
+// Riverpod providers expose Firebase Auth state to the UI layer
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../services/auth_service.dart';
